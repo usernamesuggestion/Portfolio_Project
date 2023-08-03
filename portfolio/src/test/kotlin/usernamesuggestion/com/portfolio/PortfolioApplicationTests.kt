eb5350f3d63d9c8e20d9cb00fe4d2956c4e9ebdb
Adam Wilson <adamwilson@usernamesuggestion-MBP2023-2.local>
2023-08-03 09:10:35 -0700

    03082023SpringBootAdd

---
+++ portfolio/src/test/kotlin/usernamesuggestion/com/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package usernamesuggestion.com.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
